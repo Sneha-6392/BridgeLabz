cdbdd3771e33882ee2050f05e086455d386edb95
Sneha-6392 <sneha.mauryasm02@gmail.com>
2025-08-19 08:54:39 +0530

    ques2

--- assignment2/Main.java
+++ assignment2/Main.java
@@ -1,8 +1,15 @@
 package BridgeLabz.assignment2;
 // 1. Create a class Car with instance variables model and year. Create two car objects and display their details.
+
+// 2. Add a static variable numberOfCars in the Car class from Q1 to count how many objects have been created.
+
 class Car {
     String model;
     int year;
+    static int numberOfCars = 0;
+    Car() {
+        numberOfCars++;
+    }   
 }
 public class Main {
     public static void main(String[] args) {
@@ -14,5 +21,6 @@ public class Main {
         c2.year = 2022;
         System.out.println(c1.model + " - " + c1.year);
         System.out.println(c2.model + " - " + c2.year);
+        System.out.println("Total number of cars created: " + Car.numberOfCars);
     }
 }
